3f10fccaac6b6fb8653d48cc91ce33007cdc14b2
Can Yakar <canyakar9797@gmail.com>
2023-12-26 18:11:17 +0300

    initial commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'
+import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+
+const Layout = () => {
+  return (
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: Colors.primary,
+      tabBarLabelStyle: {
+        fontFamily: 'mon-sb'
+      }
+    }} >
+         <Tabs.Screen
+            name='index'
+            options={{
+              tabBarLabel: 'Explore',
+              tabBarIcon: ({ color, size }) =>
+                <Ionicons  name='search' color={color} size={size} />
+            }}
+         />
+    </Tabs>
+  )
+}
+
+export default Layout
