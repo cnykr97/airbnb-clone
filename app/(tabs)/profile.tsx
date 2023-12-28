857caaa0f296daf4358ba09030aa0495b80d0a7a
Can Yakar <canyakar9797@gmail.com>
2023-12-28 11:07:45 +0300

    clerk init

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
 
 const Profile = () => {
+  const { isSignedIn, signOut } = useAuth()
   return (
     <View>
-      <Text>Profile</Text>
+      {isSignedIn ? (
+        <Button title='Sign Out' onPress={() => signOut()} />
+      ) : (
+        <Link href={'/(modals)/login'} ><Text>Login</Text></Link>
+      )}
     </View>
   )
 }
